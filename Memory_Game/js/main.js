6ebc2005951513421256796fe0e0e647e67f2bcb
John Lucker <john.lucker@comcast.net>
2020-02-13 12:15:01 -0500

    created functions for game logic

--- Memory_Game/js/main.js
+++ Memory_Game/js/main.js
@@ -1,16 +1,18 @@
-console.log("Up and running!");
 let cards = ["queen","queen","king","king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-console.log("User has flipped " + cardsInPlay[0]);
-cardsInPlay.push(cardTwo);
-console.log("User has flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}
+function checkForMatch() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		};
+};
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+flipCard(0);
+flipCard(2);
